3a18510f287e4c2560ca85175a7d0e2b171c6c1c
slxshx <leon.linss@outlook.de>
2025-06-14 13:50:34 +0200

    BaseSetup!

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
 <?php
+// Autoloading einbauen
+require __DIR__ . '../vendor/autoload.php';
+// Session starten
+session_start();
+// Router starten
+$router->get('/dashboard', 'DashboardController@index');
+$router->post('/api-request', 'ApiRequestController@store');
