389dc56f95b611f520671a7e3fb815b59c85274a
slxshx <leon.linss@outlook.de>
2025-06-24 20:44:21 +0200

    creating basic dashboard structure, brainstormed dashboard design

---
+++ app/Views/dashboard.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/dashboard.css">
+    <title>SATRACK Dashboard</title>
+</head>
+<header>
+    <div class="header--logo">
+
+    </div>
+    <div class="navbar">
+        <div class="navbar--entry">
+
+        </div>
+    </div>
+    <div class="searchbar">
+
+    </div>
+</header>
+<body>
+    <div class="sidebar">
+        <div class="sidebar--features">
+
+        </div>
+    </div>
+    <div class="dashboard--wrapper">
+        <div class="dashboard--box">
+            <div class="dashboard--content">
+                <div class="dashboard--item">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<footer>
+    <div class="footer--wrapper">
+        <div class="footer--content">
+            <div class="footer--links">
+
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
